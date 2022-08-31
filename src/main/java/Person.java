535b59cc1e7a5345c4be5f3c5370bc63aeaf75b3
Miguel Morales <miguelmorales@Miguels-MacBook-Pro.local>
2022-08-31 14:55:28 -0400

    Completed hw

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,24 @@
 public class Person {
     public static void main(String[] args) {
-        System.out.println("I got this shit!");
+        String shit = "I got this shit!";
+        System.out.println(shit);
+
+        int myNum = 12;
+        myNum = 15;  // changed value from 12 to 15 then added 4
+        System.out.println(myNum + 4);
+
+        if (5 > 3) {
+            System.out.println("5 is greater than 2");
+        }else {
+            System.out.println("5 is not greater than 2");
+        }
+
+
+        int score = 3;
+        if (score < 1) {
+            System.out.println("the team has more than 1 points");
+        } else {
+            System.out.println("the team has 3 points");
+        }
     }
 }
